2fe6e22e61cadbf3e499c89d31e446925f4f43c2
armst209 <armst209@gmail.com>
2026-05-07 05:24:52 -0400

    feat: Update Footer component to adjust height and improve text styling for better layout

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,14 +1,18 @@
+import HeroSocialLinksSection from "../Hero/HeroSocialLinksSection"
 
 const Footer = () => {
     return (
-        <footer className="flex flex-col items-center justify-center border-t-[.5px] border-t-[hsl(240,4,16)] w-full h-40 p-4 mb-6 pr-6 ">
+        <footer className="flex flex-col items-center justify-center border-t-[.5px] border-t-[hsl(240,4,16)] w-full h-auto p-4 mb-6 pr-6 ">
             <div className="flex flex-col justify-between w-full lg:flex-row">
                 <div className="flex justify-center w-full text-wrap text-center lg:justify-start">
                     <h2 className="emerald-green-text-color font-bold">{`© ${new Date().getFullYear()} Aaron Armstrong. All rights are reserved.`}</h2>
                 </div>
-                <div className="flex justify-center text-wrap text-center">
+                <div className="flex justify-center text-wrap text-center text-sm">
                     <p>Website built with Next.js, Typescript and Tailwind CSS. Icons provided by <span><a href="https://www.flaticon.com/uicons" rel="noopener noreferer" target="_blank">Flaticon</a></span></p>
                 </div>
+            </div>
+            <div className="flex justify-center lg:justify-start w-full">
+                <HeroSocialLinksSection />
             </div>
 
         </footer>
