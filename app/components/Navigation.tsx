a47a3698575b27b1a4fa5176f73d93a428018887
armst209 <armst209@gmail.com>
2026-05-07 17:54:25 -0400

    updating mobile menu padding top

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -34,9 +34,9 @@ const Navigation = () => {
                     <button id="hamburger" className="flex lg:hidden" onClick={toggleMobileMenu} aria-label="Open Navigation">
                         <Image src={HamburgerIcon} width={25} alt="mobile menu icon" className="icon-white-filter" />
                     </button>
-                    <div role="dialog" id="mobile-menu" aria-label="mobile menu" aria-expanded={isOpen} className={`w-full h-screen flex fixed inset-0 bg-background z-50 transition-transform duration-300 ease-out mt-[3.6rem] shadow(12px 9px 18px 5px rgba(0,0,0,0.84)) transform ${isOpen ? " translate-x-0" : "translate-x-full"} lg:hidden`}>
+                    <div role="dialog" id="mobile-menu" aria-label="mobile menu" aria-expanded={isOpen} className={`w-full h-screen flex fixed inset-0 bg-background z-50 transition-transform duration-300 ease-out  shadow(12px 9px 18px 5px rgba(0,0,0,0.84)) transform ${isOpen ? " translate-x-0" : "translate-x-full"} lg:hidden`}>
                         <Image src={BackgroundSwirls} alt="background image swirls" fill style={{ objectFit: "cover" }} className="absolute inset-0" priority fetchPriority="high" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
-                        <button onClick={closeMobileMenu} className="absolute top-0 flex justify-end w-full pt-2 pr-3" aria-label="Close Navigation">
+                        <button onClick={closeMobileMenu} className="absolute top-0 flex justify-end w-full pt-5 pr-3" aria-label="Close Navigation">
                             <Image src={CloseIcon} alt="close navigation icon" width={40} className="icon-white-filter" />
                         </button>
                         <div className="z-10 relative font-bold text-6xl flex flex-col pt-20 pl-4 h-full">
