97604a98245a84932ed4941c9adeb3cd6136b5f2
armst209 <armst209@gmail.com>
2026-05-10 02:03:31 -0400

    feat: Improve layout and spacing in UIUXSection component

--- app/components/UIUXSection.tsx
+++ app/components/UIUXSection.tsx
@@ -7,9 +7,9 @@ import SemiColonPeriodScribble from "./SemiColonPeriodScribble"
 const UIUXSection = () => {
     return (
         <section className="pt-5 pb-6 pl-4 pr-4 w-full flex justify-center">
-            <div className="w-full h-auto flex flex-col items-center justify-evenly border-[.5px] border-[hsl(240,4,16)] rounded-xl bg-background lg:w-[80%] xl:flex-row xl:h-70">
-                <div className="w-full flex pb-6 flex-col p-4 xl:w-[33.3%] lg:flex-row">
-                    <div className="flex items-center justify-center pb-6 lg:justify-start">
+            <div className="w-full h-auto flex flex-col items-center justify-evenly border-[.5px] border-[hsl(240,4,16)] rounded-xl bg-background lg:w-[80%] 2xl:flex-row xl:h-70">
+                <div className="w-full flex pb-6 flex-col p-4 lg:flex-row 2xl:w-[34%]">
+                    <div className="flex items-center justify-center lg:justify-start">
                         <div className="z-10 relative h-20 w-20 flex items-center justify-center mr-4 lg:mr-0">
                             <div className="flex justify-center items-center">
                                 <Image src={ScribbleSmallCircle} style={{ objectFit: "cover", objectPosition: "center" }} className="absolute inset-0 scribble-color-filter" alt="small scribbled circle" />
@@ -22,17 +22,17 @@ const UIUXSection = () => {
                         <h2 className="text-lg lg:text-2xl">I am a developer first, but I have experience in design</h2>
                     </div>
                 </div>
-                <div className="z-10 w-full border-b-[.5px] border-b-[hsl(240,4,16)] xl:border-b-0 xl:border-r-[.5px] xl:border-r-[hsl(240,4,16)] xl:h-full xl:w-auto"></div>
-                <div className="flex p-4 flex-col w-full md:flex-row xl:w-[66.6%]">
-                    <div className="p-4 w-full lg:w-[50%]">
+                <div className="z-10 w-full border-b-[.5px] border-b-[hsl(240,4,16)] 2xl:border-b-0 xl:border-r-[.5px] xl:border-r-[hsl(240,4,16)] 2xl:h-full 2xl:w-auto"></div>
+                <div className="flex p-2 flex-col w-full md:flex-row xl:w-[66.6%]">
+                    <div className="p-2 grid grid-cols-1 w-full lg:w-[50%]">
                         <div className="flex flex-row items-center justify-center"><Image src={CheckIcon} alt="contact icon" width={20} className="mr-4 icon-emerald-filter" /><p className="text-lg">Wireframing & Prototyping</p></div>
                         <div className="flex flex-row items-center justify-center"><Image src={CheckIcon} alt="contact icon" width={20} className="mr-4 icon-emerald-filter" /><p className="text-lg">Typography/Font Selection</p></div>
                         <div className="flex flex-row items-center justify-center"><Image src={CheckIcon} alt="contact icon" width={20} className="mr-4 icon-emerald-filter" /><p className="text-lg">Responsive & Modern Layouts</p></div>
                     </div>
-                    <div className="p-4 flex flex-col items-center justify-start w-full lg:w-[50%]">
-                        <div className="flex flex-row items-center justify-center"><Image src={CheckIcon} alt="contact icon" width={20} className="mr-4 icon-emerald-filter" /><p className="text-lg">Product Branding/Storytelling</p></div>
-                        <div className="flex flex-row items-center justify-center"><Image src={CheckIcon} alt="contact icon" width={20} className="mr-4 icon-emerald-filter" /><p className="text-lg">User Testing</p></div>
-                        <div className="flex flex-row items-center justify-center"><Image src={CheckIcon} alt="contact icon" width={20} className="mr-4 icon-emerald-filter" /><p className="text-lg">Color Selection/Accessibility</p></div>
+                    <div className="p-2 grid grid-cols-1 w-full lg:w-[50%]">
+                        <div className="flex flex-row items-center justify-center"><Image src={CheckIcon} alt="contact icon" width={20} className="mr-4 icon-emerald-filter" /><p className="text-lg">Product Branding & Storytelling</p></div>
+                        <div className="flex flex-row items-center justify-start xxs:justify-center"><Image src={CheckIcon} alt="contact icon" width={20} className="mr-4 icon-emerald-filter" /><p className="text-lg">User Testing</p></div>
+                        <div className="flex flex-row items-center justify-start xxs:justify-center"><Image src={CheckIcon} alt="contact icon" width={20} className="mr-4 icon-emerald-filter" /><p className="text-lg">Color Selection</p></div>
                     </div>
                 </div>
             </div>
