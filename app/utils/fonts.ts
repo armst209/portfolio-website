e56aed5cbd3ca9239df3be3f44e8b2e35e65dcbb
armst209 <armst209@gmail.com>
2026-05-11 22:36:56 -0400

    feat: Update font imports and remove unused font files

--- app/utils/fonts.ts
+++ app/utils/fonts.ts
@@ -3,6 +3,11 @@ import localFont from 'next/font/local'
 export const satoshi = localFont({
     src: [
         {
+            path: '../../public/fonts/Satoshi-Light.woff2',
+            weight: '400',
+            style: 'light',
+        },
+        {
             path: '../../public/fonts/Satoshi-Regular.woff2',
             weight: '400',
             style: 'normal',
@@ -12,24 +17,16 @@ export const satoshi = localFont({
             weight: '400',
             style: 'italic',
         },
+
+        {
+            path: '../../public/fonts/Satoshi-Medium.woff2',
+            weight: '600',
+            style: 'medium',
+        },
+
         {
             path: '../../public/fonts/Satoshi-Bold.woff2',
             weight: '900',
-            style: 'bold',
-        },
-    ],
-})
-
-export const scheherazadeNew = localFont({
-    src: [
-        {
-            path: '../../public/fonts/scheherazade-new-latin-400-normal.woff2',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: '../../public/fonts/scheherazade-new-latin-700-normal.woff2',
-            weight: '700',
             style: 'bold',
         },
     ],
